761cd53c9f00044baef6b9aea6eb92f677188668
blalopsau <blalopsau@gmail.com>
2021-10-16 21:03:14 +0200

    Mackeado app

---
+++ app/src/main/java/com/example/blancolopezsaulproyectopmdm/CrearcuentaActivity.kt
@@ -0,0 +1,15 @@
+package com.example.blancolopezsaulproyectopmdm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CrearcuentaActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crearcuenta)
+
+
+    }
+}
