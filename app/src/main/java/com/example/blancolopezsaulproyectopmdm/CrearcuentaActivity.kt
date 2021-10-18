2faac117da8326302ed2f5db17264be1ee188908
blalopsau <blalopsau@gmail.com>
2021-10-18 21:09:56 +0200

    Creacion de sharedPreferences y validacion del usuario y la contraseña

--- app/src/main/java/com/example/blancolopezsaulproyectopmdm/CrearcuentaActivity.kt
+++ app/src/main/java/com/example/blancolopezsaulproyectopmdm/CrearcuentaActivity.kt
@@ -10,13 +10,13 @@ import android.content.SharedPreferences
 import android.net.Uri
 import android.util.Patterns
 import android.widget.Toast
+import com.example.blancolopezsaulproyectopmdm.MainActivity.Companion.pref
 import java.util.regex.Pattern
 
 
 class CrearcuentaActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCrearcuentaBinding
-    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,22 +30,13 @@ class CrearcuentaActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             comprobarDatos()//Comprobamos que la contraseña cumpla los parámetros
 
-            if (comprobarDatos() == true) {//Si los datos son correctos cargamos los datos en el sharedPreferences
-                sharedPreferences = getPreferences(Context.MODE_PRIVATE) ?: return@setOnClickListener
-                with(sharedPreferences.edit()) {
-                    val usuario = null
-                    val apellidos = null
-                    val email = null
-                    val contraseña = null
-                    val repContraseña = null
 
-                    putString(binding.tietNombreUsuario.text.toString(), usuario)
-                    putString(binding.tietApellidos.text.toString(), apellidos)
-                    putString(binding.tietEmail.text.toString(), email)
-                    putString(binding.tietContraseA.text.toString(), contraseña)
-                    putString(binding.tietRepetirContraseA.text.toString(), repContraseña)
-                    commit()
-                }
+            if (comprobarDatos() == true) {//Si los datos son correctos cargamos los datos en el sharedPreferences
+
+                val usuario = binding.tietNombreUsuario.text.toString()
+                val contraseña = binding.tietContraseA.text.toString()
+
+                pref.guardar(usuario, contraseña)
                 startActivity(intent)
             }
         }
@@ -61,13 +52,16 @@ class CrearcuentaActivity : AppCompatActivity() {
         val repcontraseña = binding.tietRepetirContraseA.text.toString()
 
         if (validarEmail(binding.tietEmail.text.toString()) == false) {//Si el email es incorrecto
-            val toast = Toast.makeText(this,"El email no es correcto",Toast.LENGTH_SHORT).show()//Mensaje de que el email es incorrecto
+            Toast.makeText(this, "El email no es correcto", Toast.LENGTH_SHORT)
+                .show()//Mensaje de que el email es incorrecto
             return false
-        } else if (contraseña.length <= 8 || contraseña.length >= 16) {//Comprobamos la longitud de la contraseña
-            val toastemail = Toast.makeText(this,"La contraseña no tiene la longitud correcta",Toast.LENGTH_SHORT).show()//Mensaje de que el email es incorrecto
+        } else if (contraseña.length < 8 || contraseña.length > 16) {//Comprobamos la longitud de la contraseña
+            Toast.makeText(this, "La contraseña no tiene la longitud correcta", Toast.LENGTH_SHORT)
+                .show()//Mensaje de que el email es incorrecto
             return false
         } else if (!contraseña.equals(repcontraseña)) {
-            val toast = Toast.makeText(this,"Las contraseñas no coinciden",Toast.LENGTH_SHORT).show()//Mensaje de que el email es incorrecto
+            Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT)
+                .show()//Mensaje de que el email es incorrecto
             return false
         }
         return true
--- app/src/main/java/com/example/blancolopezsaulproyectopmdm/MainActivity.kt
+++ app/src/main/java/com/example/blancolopezsaulproyectopmdm/MainActivity.kt
@@ -1,17 +1,27 @@
 package com.example.blancolopezsaulproyectopmdm
 
+
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.blancolopezsaulproyectopmdm.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    companion object {
+        lateinit var pref: Preferences
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        pref = Preferences(applicationContext)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -20,5 +30,16 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, CrearcuentaActivity::class.java)
             startActivity(intent)
         }
+
+        binding.btLogin.setOnClickListener {
+            val nombre =pref.recuperarDatos("nombre")
+            val contraseña =pref.recuperarDatos("contraseña")
+
+
+           if (!nombre.equals(binding.tietUsuario.text.toString()) || !contraseña.equals(binding.tietContrasenha.text.toString())){
+                Toast.makeText(this,"El usuario o la contraseña no existen",Toast.LENGTH_SHORT).show()
+            }
+        }
     }
+
 }
