d84aefed706b0efbdd1715574325216fd808f02a
blalopsau <blalopsau@gmail.com>
2021-11-06 15:07:56 +0100

    mejora de la visualizacion

--- app/src/main/java/com/example/blancolopezsaulproyectopmdm/activities/DetallesPeliculaActivity.kt
+++ app/src/main/java/com/example/blancolopezsaulproyectopmdm/activities/DetallesPeliculaActivity.kt
@@ -1,5 +1,6 @@
 package com.example.blancolopezsaulproyectopmdm.activities
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.blancolopezsaulproyectopmdm.R
@@ -7,14 +8,17 @@ import com.example.blancolopezsaulproyectopmdm.databinding.ActivityDetallesPelic
 import com.example.blancolopezsaulproyectopmdm.modelo.entities.Pelicula
 import com.squareup.picasso.Picasso
 import android.text.method.ScrollingMovementMethod
+import java.net.URL
+import android.content.Intent
+
 
 
 
 
 class DetallesPeliculaActivity : AppCompatActivity() {
 
-    companion object{
-        lateinit var pelicula:Pelicula
+    companion object {
+        lateinit var pelicula: Pelicula
     }
 
     private lateinit var binding: ActivityDetallesPeliculaBinding
@@ -22,19 +26,21 @@ class DetallesPeliculaActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalles_pelicula)
 
-        binding= ActivityDetallesPeliculaBinding.inflate(layoutInflater)
+        binding = ActivityDetallesPeliculaBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        pelicula=intent.extras?.get("pelicula") as Pelicula
+        pelicula = intent.extras?.get("pelicula") as Pelicula
 
         Picasso.get().load(pelicula.caratula).into(binding.ivCaratulaDetalle)
-        binding.tvTituloDetalle.text= pelicula.titulo
-        binding.tvDescripcionDetalle.text=pelicula.descripcion
-        binding.tvNotaDetalle.text=pelicula.nota
-        binding.tvGeneroDetalle.text= pelicula.genero
-        binding.tvPlataformaDetalle.text= pelicula.plataforma
-        binding.tvTiempoDetalle.text= pelicula.tiempo
-
+        binding.tvTituloDetalle.text = pelicula.titulo
+        binding.tvDescripcionDetalle.text = pelicula.descripcion
+        binding.tvNotaDetalle.text = pelicula.nota
         binding.tvDescripcionDetalle.setMovementMethod(ScrollingMovementMethod())
+        binding.tvGeneroDetalle.text = pelicula.genero
+        binding.tvPlataformaDetalle.text = pelicula.plataforma
+        binding.tvTiempoDetalle.text = pelicula.tiempo
+
+
+
     }
 }
