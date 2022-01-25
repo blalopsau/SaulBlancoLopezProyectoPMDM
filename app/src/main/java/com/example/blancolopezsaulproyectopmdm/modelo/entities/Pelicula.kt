55c01a50bafa988f203649cee0d0725b610a1def
blalopsau <blalopsau@gmail.com>
2022-01-25 09:22:03 +0100

    Actualizacion de app para conexión con API

--- app/src/main/java/com/example/blancolopezsaulproyectopmdm/modelo/entities/Pelicula.kt
+++ app/src/main/java/com/example/blancolopezsaulproyectopmdm/modelo/entities/Pelicula.kt
@@ -1,6 +1,16 @@
 package com.example.blancolopezsaulproyectopmdm.modelo.entities
 
+import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
-data class Pelicula (var titulo:String, var genero:String,var director:String,var nota:String,var plataforma:String,var tiempo:String ,var descripcion:String,var caratula:String,var tel:String) :
-    Serializable
+data class Pelicula(
+    @SerializedName("title") var titulo: String,
+    var genero: String,
+    var director: String,
+    var nota: String,
+    var plataforma: String,
+    @SerializedName("runtimeMinutes")var tiempo: String,
+    var descripcion: String,
+    var caratula: String,
+    var tel: String
+):Serializable
